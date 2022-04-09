abf6701f5b80ff9fbe8702de03a5e55afe744883
Unnati-Maheshwari <91026895+Unnati-Maheshwari@users.noreply.github.com>
2022-04-09 21:08:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function flip_image()
+{
+    document.getElementById("phone").src="pink-phone.gif"
+}
+function set_original_image()
+{
+    document.getElementById("phone").src="cell phone.jfif"
+}
